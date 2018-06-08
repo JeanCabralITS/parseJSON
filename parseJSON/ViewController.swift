bff75bc28c98277c6f8344d8c6bb23cd1c67e92b
Jean <Jean.Cabral@its.ny.gov>
2018-06-08 10:59:46 -0400

    1st Commit

--- parseJSON/ViewController.swift
+++ parseJSON/ViewController.swift
@@ -9,7 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    //MARK: Properties
+    
+    @IBOutlet weak var textJSON: UILabel!
+    
+   
+    
 
+    
+    
+    
+    //MARK: Functions
+    @IBAction func getJSON(_ sender: UIButton) {
+     print("Got!")
+        
+    }
+    
+    @IBAction func postJSON(_ sender: UIButton) {
+        print("Sent!")
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else {return}
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let postDictionary = ["userId": "55", "id": "126", "title": "Hey There","body": "What Are you doing this weekend ?"]
+        
+      
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
